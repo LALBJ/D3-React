b84e7c879f1ddd6c7dbdbd63bc2c5ee92fda0e38
LALBJ <905976755@qq.com>
2021-08-22 22:01:08 +0800

    complete part of map

--- src/components/ComposableMap.js
+++ src/components/ComposableMap.js
@@ -1,7 +1,40 @@
-const ComposableMap = ({...props}) => {
+import React from "react"
+import PropTypes from "prop-types"
+
+import { MapProvider } from "./MapProvider.js"
+const ComposableMap = ({
+    width = 800,
+    height = 600,
+    projection = "geoEqualEarth",
+    projectionConfig = {},
+    className = "",
+    ...restProps
+}) => {
     return (
-        <svg {...props} />
+        <MapProvider
+            width={width}
+            height={height}
+            projection={projection}
+            projectionConfig={projectionConfig}
+        >
+            <svg    
+                viewBox={`0 0 ${width} ${height}`}
+                className={`rsm-svg ${className}`}
+                {...restProps}
+            />
+        </MapProvider>
     )
+}
+
+ComposableMap.propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    projection: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func
+    ]),
+    projectionConfig: PropTypes.object,
+    className: PropTypes.string,
 }
 
 export default ComposableMap
--- src/components/Geographies.js
+++ src/components/Geographies.js
@@ -1,4 +1,18 @@
-const Geographies = ({}) => {
+import React, { useContext } from "react"
+import PropTypes from "prop-types"
+
+import { MapContext } from "./MapProvider"
+import useGeographies from "./useGeographies"
+
+const Geographies = ({
+    geography,
+    children,
+    parseGeographies,
+    className = "",
+    ...restProps
+}) => {
+    const { path, projection } = useContext(MapContext)
+    const { georaphies,  }
     return (
         <g>
             <rect width="200px" height="200px"/>
